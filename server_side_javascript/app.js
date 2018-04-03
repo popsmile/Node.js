53f50f17e9d5434c23a75239f1cbec1681a9a27b
Jiyeong Yun <popsmile7@gmail.com>
2018-04-03 17:11:23 +0900

    Upload how to use static files

--- server_side_javascript/app.js
+++ server_side_javascript/app.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
     res.send('Hello World!');
 });
-
+app.get('/route', function(req, res){
+    res.send('Hello Router, <img src="/kitty.png">');
+});
 app.get('/login', function(req, res){
     res.send('<h1>Login Please</h1>');
 });
-
 app.listen(3000, function(){
     console.log('Example app listening on port 3000!');
 });
